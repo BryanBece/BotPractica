a5edfe0f0aa3034db050b692faffb7ad0384e431
Bryanbece <br.becerra.p@gmail.com>
2024-01-12 16:31:38 -0300

    Refactor home.html and views.py

--- botApp/views.py
+++ botApp/views.py
@@ -9,81 +9,36 @@ from io import BytesIO
 import base64
 
 
-# def generar_grafico_lineas_ingresos():
-#     with connection.cursor() as cursor:
-#         cursor.execute('SELECT COUNT(*), fecha_ingreso FROM botApp_respuesta GROUP BY fecha_ingreso')
-#         resultados = cursor.fetchall()
+def generar_grafico_lineas_ingresos():
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT COUNT(*) FROM botApp_usuario')
+        total_usuarios = cursor.fetchone()[0]
 
-#     categorias = [str(result[1].strftime('%d/%m/%Y')) for result in resultados]
-#     valores = [result[0] for result in resultados]
+    categorias = ['Total Usuarios']
+    valores = [total_usuarios]
 
-#     plt.plot(categorias, valores, marker='o', linestyle='-', color='blue')
-#     plt.xlabel('Fechas de Ingreso')
-#     plt.ylabel('Número de Registros')
-#     plt.title('Gráfico de Líneas')
+    plt.plot(categorias, valores, marker='o', linestyle='-', color='blue')
+    plt.xlabel('Categorías')
+    plt.ylabel('Número de Usuarios')
+    plt.title('Gráfico de Líneas')
 
-#     buffer = BytesIO()
-#     plt.savefig(buffer, format='png')
-#     buffer.seek(0)
-#     plt.close()
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
 
-#     imagen_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-#     return imagen_base64
+    imagen_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return imagen_base64
 
-
-# def generar_grafico_lineas_genero():
-#     with connection.cursor() as cursor:
-#         cursor.execute('SELECT genero_id, COUNT(*) FROM botApp_respuesta GROUP BY genero_id')
-#         resultados = cursor.fetchall()
-
-#     generos = [Genero.objects.get(id=result[0]).nombre for result in resultados]
-#     cantidades = [result[1] for result in resultados]
-
-#     plt.plot(generos, cantidades, marker='o', linestyle='-', color='green')
-#     plt.xlabel('Género')
-#     plt.ylabel('Número de Registros')
-#     plt.title('Gráfico de Líneas por Género')
-
-#     buffer = BytesIO()
-#     plt.savefig(buffer, format='png')
-#     buffer.seek(0)
-#     plt.close()
-
-#     imagen_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-#     return imagen_base64
-
-
-# def generar_grafico_lineas_ingresos_por_mes():
-#     with connection.cursor() as cursor:
-#         cursor.execute('SELECT DATE_FORMAT(fecha_ingreso, "%Y-%m") AS mes, COUNT(*) FROM botApp_respuesta GROUP BY mes ORDER BY mes')
-#         resultados = cursor.fetchall()
-
-#     meses = [result[0] for result in resultados]
-#     cantidades = [result[1] for result in resultados]
-
-#     plt.plot(meses, cantidades, marker='o', linestyle='-', color='purple')
-#     plt.xlabel('Mes')
-#     plt.ylabel('Número de Ingresos')
-#     plt.title('Ingresos por Mes')
-
-#     buffer = BytesIO()
-#     plt.savefig(buffer, format='png')
-#     buffer.seek(0)
-#     plt.close()
-
-#     imagen_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-#     return imagen_base64
 
 
 @login_required
 def home(request):
-    #Datos = Respuesta.objects.all()
+    Datos = Usuario.objects.all()
     data = {
         'form': UsuarioForm(),
-        #'Datos': Datos,
-        #'imagen_base64_ingresos': generar_grafico_lineas_ingresos(),
-        #'imagen_base64_genero': generar_grafico_lineas_genero(),
-        #'imagen_base64_ingresos_por_mes': generar_grafico_lineas_ingresos_por_mes(),
+        'Datos': Datos,
+        'imagen_base64_ingresos': generar_grafico_lineas_ingresos(),
     }
 
     if request.method == 'POST':
