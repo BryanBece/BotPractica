59d8adf638ee09327d32468f17c86f0c560de96e
Bryanbece <br.becerra.p@gmail.com>
2024-01-15 08:35:18 -0300

    Añadir administrador de preguntas

--- botApp/admin.py
+++ botApp/admin.py
@@ -10,5 +10,13 @@ class UsuarioAdmin(admin.ModelAdmin):
     list_filter = ('id', 'anioNacimiento', 'Comuna_Usuario', 'Genero_Usuario', 'SistemaSalud_Usuario',
                    'Ocupacion_Usuario')
 
+                   
+class PreguntaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pregunta')
+    search_fields = ('id', 'pregunta')
+    list_filter = ('id', 'pregunta')
+
 
 admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Pregunta, PreguntaAdmin)
+
